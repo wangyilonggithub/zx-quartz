c3b5469b1a54ad12ab6a559d358e970f2a645691
ityouknow <ityouknow@126.com>
2017-06-27 15:16:41 +0800

    init

---
+++ src/main/java/com/zhongxin/quartz/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.zhongxin.quartz.controller;
+
+import com.icardpay.component.common.log.Logger;
+import com.icardpay.component.common.log.LoggerFactory;
+
+public class BaseController {
+	
+	protected Logger logger =LoggerFactory.getLogger(this.getClass());
+
+}
